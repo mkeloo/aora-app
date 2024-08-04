c7cde288671a86062da0077111265f6b73b85b5d
mkeloo <mkeloo@ufl.edu>
2024-08-04 03:12:28 -0400

    feat: Add Trending component, FormField, CustomButton, InfoBox, Loader, SearchInput, and EmptyState components

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, Image, Alert } from 'react-native';
+import { View, Text, FlatList, Image, RefreshControl } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import SearchInput from '../../components/SearchInput';
-import Trending from '../../components/Trending';
 
 import { images } from '../../constants';
+
+import useAppwrite from '../../lib/useAppwrite';
+import { getAllPosts, getLatestPosts } from '../../lib/appwrite';
+
 import EmptyState from '../../components/EmptyState';
-import { getAllPosts } from '../../lib/appwrite';
+import SearchInput from '../../components/SearchInput';
+import Trending from '../../components/Trending';
 import VideoCard from '../../components/VideoCard';
 
 const Home = () => {
   const { data: posts, refetch } = useAppwrite(getAllPosts);
+  const { data: latestPosts } = useAppwrite(getLatestPosts);
+
   const [refreshing, setRefreshing] = useState(false);
 
   const onRefresh = async () => {
@@ -18,16 +23,24 @@ const Home = () => {
     await refetch();
     setRefreshing(false);
   };
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <FlatList
-        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
-        // data={ []}
+        data={posts}
         keyExtractor={(item) => item.$id}
-        renderItem={({ item }) => <VideoCard video={item} />}
+        renderItem={({ item }) => (
+          <VideoCard
+            title={item.title}
+            thumbnail={item.thumbnail}
+            video={item.video}
+            creator={item.creator.username}
+            avatar={item.creator.avatar}
+          />
+        )}
         ListHeaderComponent={() => (
-          <View className="my-6 px-4 space-y-6">
-            <View className="justify-between items-start flex-row mb-6">
+          <View className="flex my-6 px-4 space-y-6">
+            <View className="flex justify-between items-start flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">
                   Welcome Back
@@ -49,17 +62,18 @@ const Home = () => {
             <SearchInput />
 
             <View className="w-full flex-1 pt-5 pb-8">
-              <Text className="text-gray-100   text-lg font-pregular mb-3">
+              <Text className="text-lg font-pregular text-gray-100 mb-3">
                 Latest Videos
               </Text>
-              <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? {}} />
+
+              <Trending posts={latestPosts ?? []} />
             </View>
           </View>
         )}
         ListEmptyComponent={() => (
           <EmptyState
             title="No Videos Found"
-            subtitle="Be the first one to upload a video"
+            subtitle="No videos created yet"
           />
         )}
         refreshControl={
